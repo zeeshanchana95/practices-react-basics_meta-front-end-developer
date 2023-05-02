2aac468ba0ed8786d8479d65c879f03262608e06
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-05-02 19:06:04 +0500

    Add Video Practice Files

---
+++ PR05 Videos/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactPlayer from "react-player/youtube";
+
+const App = () => {
+  return (
+    <div>
+      <MyVideo />
+    </div>
+  );
+};
+
+const MyVideo = () => {
+  return (
+    <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+  );
+};
+
+export default App;
